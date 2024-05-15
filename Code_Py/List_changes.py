e636732f28bfd4fe00860d38252c5b3272ea9a86
KuduoKid <151035598+KuduoKid@users.noreply.github.com>
2024-05-15 11:50:01 +0800

    Add files via upload

---
+++ Code_Py/List_changes.py
@@ -0,0 +1,28 @@
+
+name = ['a', 'b', 'c']
+changes = 'a'
+name.remove(changes)
+
+invite = ("inviting you to dinner together")
+for inviting in name:
+    print(inviting, ",", invite)
+
+print(f"Unfortunately,  {changes} is unable to attend dinner.")
+# f: 
+
+
+#方法remove()用法
+"""
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+
+
+changes = name.remove('a') 
+方法 remove() 无返回值
+
+print(changes)
+print("Unfortunately, " changes " is unable to attend dinner.")
+所以打印结果为:Unfortunately, None is unable to attend dinner.
+
+"""
---
+++ Code_Py/add_Guests.py
@@ -0,0 +1,8 @@
+name = ['a', 'b', 'c', 'd', 'e', 'f']
+invite = ("inviting you to dinner together")
+for inviting in name:
+    print(invite,",", inviting)
+
+print("I found a bigger dining table")
+
+#print(invite, "," inviting)
