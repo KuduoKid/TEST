840022349212be47b21d0611e733297d237e473c
KuduoKid <151035598+KuduoKid@users.noreply.github.com>
2024-05-24 13:53:06 +0800

    Add files via upload

---
+++ Code_Py/5.3.alien1.py
@@ -0,0 +1,29 @@
+"""
+alien_color = ['green', 'yellow', 'red']
+color = 'green'
+#color = 'greenn'
+if color == 'green':
+    print("You got 5 points!")
+elif color != 'green':
+    print("You got 10 points!")
+"""
+#else代码块
+"""
+alien_color = ['green', 'yellow', 'red']
+color = 'green'
+#color = 'greenn'
+if color == 'green':
+    print("You got 5 points!")
+else:
+    color != 'green'
+    print("You got 10 points!")
+"""
+
+
+
+
+
+
+
+
+
